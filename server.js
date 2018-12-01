e7fe7d40f8fad8f2e3457cd14df00fd4ff2ae2ef
Brian Huston <hbriandrums@gmail.com>
2018-11-30 19:41:33 -0800

    Another loader issue

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(compression());
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/loaderio-bf9c82571ab2814e37c515bd8f8ccc0c/', (req, res) => {
+app.get('/loaderio-bf9c82571ab2814e37c515bd8f8ccc0c', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/loaderio-bf9c82571ab2814e37c515bd8f8ccc0c.txt'));
 });
 
